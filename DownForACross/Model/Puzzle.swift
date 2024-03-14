1f97234dd62b94caacaf4aba5a95d2bca43d3cf1
Justin Hill <jhill.d@gmail.com>
2024-03-14 10:24:35 -0400

    - fix clue decoding for puzzles whose clues are included as dictionaries   for some reason

--- DownForACross/Model/Puzzle.swift
+++ DownForACross/Model/Puzzle.swift
@@ -55,4 +55,47 @@ struct PuzzleInfo: Codable, Hashable {
 struct PuzzleClues: Codable, Hashable {
     let across: [String?]
     let down: [String?]
+    
+    init(across: [String?], down: [String?]) {
+        self.across = across
+        self.down = down
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let acrossList = try? container.decode([String?].self, forKey: .across) {
+            self.across = acrossList
+        } else if let acrossDict = try? container.decode([String: String].self, forKey: .across) {
+            guard let maxClueNumber = acrossDict.keys.compactMap({ Int($0) }).sorted().last else {
+                throw NSError(domain: "PuzzleParsingDomain", code: 0)
+            }
+            
+            var list: [String?] = Array(repeating: nil, count: maxClueNumber + 1)
+            for (key, value) in acrossDict {
+                guard let index = Int(key) else { throw NSError(domain: "PuzzleParsingDomain", code: 1) }
+                list[index] = value
+            }
+            self.across = list
+        } else {
+            throw NSError(domain: "PuzzleParsingDomain", code: 2)
+        }
+        
+        if let downList = try? container.decode([String?].self, forKey: .down) {
+            self.down = downList
+        } else if let downDict = try? container.decode([String: String].self, forKey: .down) {
+            guard let maxClueNumber = downDict.keys.compactMap({ Int($0) }).sorted().last else {
+                throw NSError(domain: "PuzzleParsingDomain", code: 3)
+            }
+            
+            var list: [String?] = Array(repeating: nil, count: maxClueNumber + 1)
+            for (key, value) in downDict {
+                guard let index = Int(key) else { throw NSError(domain: "PuzzleParsingDomain", code: 4) }
+                list[index] = value
+            }
+            self.down = list
+        } else {
+            throw NSError(domain: "PuzzleParsingDomain", code: 5)
+        }
+    }
 }
